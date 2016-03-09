16f96638917b803f7177a1d92cb9fe1deafefc76
Bruno Rodrigues dos Santos <bruno.rodrigues171996@gmail.com>
2016-03-09 15:09:48 -0300

    qqr coisa

--- src/projeto/banco/poo/app/AppBanco.java
+++ src/projeto/banco/poo/app/AppBanco.java
@@ -4,7 +4,7 @@
 package projeto.banco.poo.app;
 
 import projeto.banco.poo.db.DbPrimeiraConexao;
-//aqui é só para teste, quando você alterar 2
+
 /**
  * @author Bruno Rodrigues dos Santos, Eric Fonseca Lima
  * @since 5 de mar de 2016
